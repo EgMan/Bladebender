3fc783156451ef0e7d4b1907313b6ca7213c7ebb
Antonio Woods <antonio95woods@gmail.com>
2020-05-09 12:16:45 -0400

    Initial commit
    Movement system outlined

---
+++ Assets/Scripts/CameraFollow.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraFollow : MonoBehaviour
+{
+    private Vector2 velocity;
+    public float smoothTimeY, smoothTimeX;
+
+    public GameObject toFollow;
+    void FixedUpdate()
+    {
+        float posX = Mathf.SmoothDamp(transform.position.x, toFollow.transform.position.x, ref velocity.x, smoothTimeX);
+        float posY = Mathf.SmoothDamp(transform.position.y, toFollow.transform.position.y, ref velocity.y, smoothTimeX);
+        transform.position = new Vector3(posX, posY, transform.position.z);
+    }
+}
---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,110 @@
+﻿using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    public Rigidbody2D body;
+
+    public float initialJumpBoost = 50f;
+    public float jumpForce = 200f;
+    public float runForce = 500f;
+    private Vector2 jumpVector;
+    private Vector2 runVector;
+    private float jumpScalar;
+    private float runScalar;
+
+    private int collisionCount = 0;
+    private bool initialJump = false;
+    private Vector2 up = new Vector2(0f, 1f);
+    void Start()
+    {
+        jumpVector = new Vector2(0, jumpForce);
+        runVector = new Vector2(runForce, 0);
+        jumpScalar = runScalar = 0f;
+    }
+    void FixedUpdate()
+    {
+        if (jumpScalar != 0f)
+        {
+            if (initialJump)
+            {
+                body.AddForce(initialJumpBoost * Time.deltaTime * jumpScalar * jumpVector);
+                initialJump = false;
+            }
+            else
+            {
+                body.AddForce(Time.deltaTime * jumpScalar * jumpForce * up);
+            }
+        }
+        if (runScalar != 0f)
+        {
+            body.AddForce(Time.deltaTime * runScalar * runVector);
+        }
+    }
+    //TODO add force add to fixedupdate
+    void Update()
+    {
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            runScalar = 1f;
+        }
+        else if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            runScalar = -1f;
+        }
+        else
+        {
+            runScalar = 0f;
+        }
+        if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.W) || Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            if (collisionCount != 0)
+            {
+                jumpScalar = 1f;
+                initialJump = true;
+            }
+        }
+        else if (Input.GetKey(KeyCode.Space) || Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            if (collisionCount != 0)
+            {
+                jumpScalar = 1f;
+            }
+        }
+        else
+        {
+            jumpScalar = 0f;
+        }
+    }
+    void OnCollisionStay2D(Collision2D other)
+    {
+        Vector2 furthestUp = other.contacts[0].normal;
+        float dotProduct = Vector2.Dot(up, furthestUp);
+        for (int i = 1; i < other.contacts.Length; i++)
+        {
+            Vector2 upCandidate = other.contacts[i].normal;
+            float upCandidateDotProduct = Vector2.Dot(furthestUp, up);
+            if (upCandidateDotProduct > dotProduct)
+            {
+                furthestUp = upCandidate;
+                dotProduct = upCandidateDotProduct;
+            }
+        }
+        float rad = Mathf.Atan2(furthestUp.x, furthestUp.y);
+        if (Mathf.Abs(rad * Mathf.Rad2Deg) < 91f)
+        {
+            furthestUp = angleUp(furthestUp, rad);
+        }
+        jumpVector = jumpForce * furthestUp;
+        collisionCount = other.contactCount;
+    }
+    void OnCollisionExit2D(Collision2D other)
+    {
+        collisionCount--;
+    }
+
+    private Vector2 angleUp(Vector2 vector, float rad)
+    {
+        float rotate = -0.55f * rad;
+        return new Vector2(Mathf.Sin(rad+rotate), Mathf.Cos(rad+rotate));
+    }
+}
