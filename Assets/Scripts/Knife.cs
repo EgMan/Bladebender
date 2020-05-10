4fac625d3d849c901b78e4ce8a1bb5eae2f0371f
Antonio Woods <antonio95woods@gmail.com>
2020-05-10 05:09:36 -0400

    Add knife projectile
    Needs cleanup

---
+++ Assets/Scripts/Knife.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Knife : MonoBehaviour
+{
+    public Rigidbody2D rb;
+    public float speed = 50f;
+    private float angleOfAttack;
+    private GameObject stuck;
+    private Vector3 localStuckPosition;
+
+    void Start()
+    {
+        rb = this.GetComponent<Rigidbody2D>();
+    }
+    void FixedUpdate()
+    {
+        if (stuck != null)
+        {
+            transform.position = stuck.transform.position + localStuckPosition;
+            rb.rotation = angleOfAttack;
+        }
+    }
+    public void throw_(Vector2 vect)
+    {
+        rb.AddForce(speed*vect);
+        angleOfAttack = rb.rotation;
+    }
+    private void OnCollisionEnter2D(Collision2D other)
+    {
+        print("knife collision");
+        if (other.collider.tag == "Player" || other.collider.tag == "Throwable")
+        {
+            stuck = null;
+            return;
+        }
+        if (stuck != null && other.collider.gameObject != stuck)
+        {
+            stuck = null;
+            return;
+        }
+        // rb.isKinematic = false;
+
+
+        // transform.parent = other.transform;
+
+        rb.velocity = Vector2.zero;
+        stuck = other.collider.gameObject;
+        // rb.isKinematic = true;
+        localStuckPosition = transform.position - stuck.transform.position;
+
+        // rb.rotation = angleOfAttack;
+        // transform.parent = other.transform;
+    }
+}
---
+++ Assets/Scripts/PlayerObjectThrowing.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerObjectThrowing : MonoBehaviour
+{
+    public GameObject throwable;
+    public Transform throwFrom;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 shootFromPos = throwFrom.transform.position;
+            Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            Vector3 shootingVector = new Vector2(
+                mousePos.x - throwFrom.transform.position.x,
+                mousePos.y - throwFrom.transform.position.y);
+            shootingVector /= shootingVector.magnitude;
+            // Quaternion rotation = Quaternion.AngleAxis(Mathf.Atan2(shootingVector.x,shootingVector.y), Vector3.forward);
+
+            mousePos.z = shootFromPos.z; // ensure there is no 3D rotation by aligning Z position
+
+            // vector from this object towards the target location
+            Vector3 vectorToTarget = mousePos - shootFromPos;
+
+            // get the rotation that points the Z axis forward, and the Y axis 90 degrees away from the target
+            // (resulting in the X axis facing the target)
+            Quaternion targetRotation = Quaternion.LookRotation(forward: Vector3.forward, upwards: vectorToTarget);
+
+            GameObject obj = Instantiate(throwable, throwFrom.position, targetRotation);
+            obj.GetComponent<Knife>().throw_(shootingVector);
+        }
+    }
+}
