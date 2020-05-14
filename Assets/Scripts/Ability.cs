9cdaabbfd18669a0ba8ca0f52855cec321f6c471
Antonio Woods <antonio95woods@gmail.com>
2020-05-14 06:21:33 -0400

    Document Ability interface
    Tele-Throw draft 2

--- Assets/Scripts/Ability.cs
+++ Assets/Scripts/Ability.cs
@@ -2,10 +2,34 @@ using UnityEngine;
 using System.Collections.Generic;
 public abstract class Ability : MonoBehaviour
 {
+    /// <summary>
+    /// Return the in-game name for this ability
+    /// </summary>
     public abstract string getName();
+
+    /// <summary>
+    /// Called once when ability is first activated
+    /// </summary>
     public abstract void activate(Knife projectile);
-    public virtual void activateShift(Knife projectile){}
-    public virtual void update(Knife projectile) {}
-    public virtual void updateHold(Knife projectile) {}
-    public virtual void release(Knife projectile) {}
+
+    /// <summary>
+    /// Called once ability is first activated (shift evokes alternate functionality)
+    /// TODO implement this. Currently doesn't function as stated above.
+    /// </summary>
+    public virtual void activateShift(Knife projectile) { }
+
+    /// <summary>
+    /// Called on every frame for knives which are still affected by the ability, but the ability is no longer being held down.
+    /// </summary>
+    public virtual void update(Knife projectile) { }
+
+    /// <summary>
+    /// Called on every frame for knives which are still affected by the ability, and only while the ability is still being held down.
+    /// </summary>
+    public virtual void updateHold(Knife projectile) { }
+
+    /// <summary>
+    /// Called once ability is released.
+    /// </summary>
+    public virtual void release(Knife projectile) { }
 }
--- Assets/Scripts/AbilityTeleThrow.cs
+++ Assets/Scripts/AbilityTeleThrow.cs
@@ -6,13 +6,25 @@ public class AbilityTeleThrow : Ability
 {
     public float translateSpeed = 1.75f;
     public float rotateSpeed = 1f;
+    public int mouseDataPoints = 100;
+
+    private Dictionary<Knife, Vector3> offsets = new Dictionary<Knife, Vector3>();
+    // private Vector3 lastMousePos = Vector3.zero;
+    private Vector3 deltaMouseVector = Vector3.zero;
+    private Queue<Vector3> mousePoints = new Queue<Vector3>();
     public override string getName()
     {
         return "Tele-Throw";
     }
     public override void activate(Knife projectile)
     {
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        offsets[projectile] = projectile.transform.position - mousePos;
+
         projectile.state = Knife.States.AntiGrav;
+
+        // lastMousePos = mousePos;
+    //    mousePoints.Enqueue(mousePos);
     }
     public override void updateHold(Knife k)
     {
@@ -23,14 +35,30 @@ public class AbilityTeleThrow : Ability
 
         Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
 
-        //interpolate translation
-        Vector2 currnt_velocity = k.rb.velocity;
-        k.rb.position = Vector2.Lerp(k.rb.position, mousePos, Time.deltaTime * translateSpeed);
 
-        //interpolate rotation
-        Vector2 vectorToTarget = mousePos - k.transform.position;
-        Quaternion rotateTo = Quaternion.LookRotation(forward: Vector3.forward, upwards: vectorToTarget);
-        k.transform.rotation = Quaternion.Lerp(rotateTo, transform.rotation, Time.deltaTime * rotateSpeed);
+        //interpolate translation
+        Vector3 offset;
+        if (!offsets.TryGetValue(k, out offset))
+        {
+            offset = Vector2.zero;
+        }
+        Vector2 currnt_velocity = k.rb.velocity;
+        k.rb.position = Vector2.Lerp(k.rb.position, mousePos+offset, Time.deltaTime * translateSpeed);
+
+        // interpolate rotation
+        // Vector2 vectorToTarget = mousePos - k.transform.position;
+        // if (vectorToTarget.magnitude > .1f)
+        // {
+        //     Quaternion rotateTo = Quaternion.LookRotation(forward: Vector3.forward, upwards: vectorToTarget);
+        //     k.transform.rotation = Quaternion.Lerp(rotateTo, transform.rotation, Time.deltaTime * rotateSpeed);
+        // }
+
+        if (deltaMouseVector.magnitude > 0.01f)
+        {
+            Quaternion rotateTo = Quaternion.LookRotation(forward: Vector3.forward, upwards: deltaMouseVector);
+            k.transform.rotation = Quaternion.Lerp(rotateTo, transform.rotation, Time.deltaTime * rotateSpeed/5000);
+        }
+
     }
 
     public override void release(Knife k)
@@ -39,5 +67,17 @@ public class AbilityTeleThrow : Ability
         k.rb.angularVelocity = 0f;
         Vector3 throwVector = k.transform.rotation * Vector3.up;
         k.launch(throwVector, 0f);
+    }
+
+    void Update()
+    {
+        // TODO abstract this logic into a mouseDirectionSmoother class
+        Vector3 mousePos = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        mousePoints.Enqueue(mousePos);
+        deltaMouseVector = mousePos - mousePoints.Peek();
+        if (mousePoints.Count > mouseDataPoints)
+        {
+            mousePoints.Dequeue();
+        }
     }
 }
